ec33b34d37e085d0b907e4e46dbb5a8acad86b57
akarasev-git <akarasev21@yandex.ru>
2025-02-11 22:45:47 +0300

    first commit

---
+++ Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Main{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(44, 12, 88, 32, 67, 48, 100, 0, -200, 666, 10000000));
+        System.out.println(list);
+        Heap.heapSort (list);
+
+        System.out.println(list);
+    }
+}
